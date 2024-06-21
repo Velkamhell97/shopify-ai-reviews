2280522a63b6fb18f43231fef24cedc718109718
Daniel Valencia <danielvalencia97@gmail.com>
2024-06-21 12:59:13 -0500

    refactor

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,9 +1,9 @@
 require('esbuild').build({
   entryPoints: ['src/ai-reviews.js'],
-  outfile: "dist/ai-reviews.min.js",
+  outfile: "dist/ai-reviews.js",
   // outdir: "dist",
   bundle: true,
-  minify: true,
+  // minify: true,
   // treeShaking: true,
   // legalComments: "linked"
 }).catch(() => process.exit(1))
