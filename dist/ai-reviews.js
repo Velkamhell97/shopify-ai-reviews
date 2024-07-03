4a19d702d580e48841780f2bc43a1a70c47dd448
Daniel Valencia <danielvalencia97@gmail.com>
2024-07-03 11:46:55 -0500

    aireviews

--- dist/ai-reviews.js
+++ dist/ai-reviews.js
@@ -224,7 +224,7 @@
           video.muted = true;
           video.src = url;
           video.onloadeddata = () => {
-            video.onloadeddata = null;
+            console.log("entre");
             const context = canvas.getContext("2d");
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
