10b0ee4676e9f5e85bf04358e4e32fb8bcfc7138
Daniel Valencia <danielvalencia97@gmail.com>
2024-07-05 16:29:28 -0500

    aireviews

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,8 +1,8 @@
 require('esbuild').build({
   entryPoints: ['src/ai-reviews.js'],
-  outfile: "dist/ai-reviews.min.js",
+  outfile: "dist/ai-reviews.js",
   bundle: true,
-  minify: true,
+  // minify: true,
   // treeShaking: true,
   // legalComments: "linked"
 }).catch(() => process.exit(1))
